e49da53345210132fa832b62fc13acf342d87ebb
Aleksander <aleksander.atamanczuk@gmail.com>
2022-05-29 13:43:47 +0200

    refactor

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { Link, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 
 import { Footer, Header } from 'components';
 import { AboutPage, HomePage } from 'pages';
