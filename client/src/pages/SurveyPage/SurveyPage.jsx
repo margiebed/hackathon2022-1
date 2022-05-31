82eb507225ccc6579e2a8c21f5c21cedf9dee752
Aleksander <aleksander.atamanczuk@gmail.com>
2022-06-01 01:33:57 +0200

    add responsive

--- client/src/pages/SurveyPage/SurveyPage.jsx
+++ client/src/pages/SurveyPage/SurveyPage.jsx
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
+import clsx from 'clsx';
 
 import styles from './surveyPage.module.scss';
 import { Sidebar } from 'components';
 
 export const SurveyPage = () => {
+  const [hideSidebar, setHideSidebar] = useState(true);
+  const leftStyle = clsx(styles.left, { [styles.hide]: hideSidebar });
+  const rightStyle = clsx(styles.content, { [styles.rightMove]: hideSidebar });
+
+  const onSidebarToggle = () => setHideSidebar((prev) => !prev);
+
   return (
     <main className={styles.survey}>
-      <Sidebar />
-      <div className={styles.content}>Survery</div>
+      <div className={rightStyle}>Survery</div>
+      <div className={leftStyle}>
+        <Sidebar hide={hideSidebar} />
+        <button className={styles.btn} onClick={onSidebarToggle}>
+          hide
+        </button>
+      </div>
     </main>
   );
 };
