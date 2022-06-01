ae48f17a5f77a1137ad6c63ad5482b9871253dea
Marta Probierz <marta.anna.probierz@gmail.com>
2022-06-01 11:12:56 +0200

    features: add survey.json

--- client/src/pages/HomePage/HomePage.jsx
+++ client/src/pages/HomePage/HomePage.jsx
@@ -1,11 +1,14 @@
+import { useTranslation } from 'react-i18next';
+
 import styles from './homePage.module.scss';
 import { Counter, Input } from 'components';
 
 export const HomePage = () => {
+  const { t } = useTranslation(['survey']);
   return (
     <main className={styles.home}>
       <Counter />
-      <Input type="mail" placeholder="Tu wpisz link" />
+      <Input type="mail" placeholder={t`survey1.linkPlaceholder`} />
     </main>
   );
 };
